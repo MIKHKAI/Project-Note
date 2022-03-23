f853fcf75dc3e3ec12736da3fe390ebe7d133f51
Mikhail <stik326@mail.ru>
2022-03-23 23:08:56 +0300

    Installed additional packages

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
