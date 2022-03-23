4046cab5656c22f7a8ba91e917bbd64c16d088b2
Mikhail <stik326@mail.ru>
2022-03-24 00:18:00 +0300

    Add event listening to the button

--- public/app.js
+++ public/app.js
@@ -1,3 +1,8 @@
 var dataF = {}
 let $card = document.querySelector('#card')
 const submit = document.querySelector('button[type="submit"]')
+
+submit.addEventListener('click', (e) => {
+    
+    
+});
