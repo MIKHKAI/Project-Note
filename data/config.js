4953fdd55f7c9560ee772380e23e1cb024a399ca
Mikhail <stik326@mail.ru>
2022-03-23 23:22:55 +0300

    Create folders partials, layouts and create file index, footer, head

--- data/config.js
+++ data/config.js
@@ -7,3 +7,33 @@ const config = {
     database: 'test',
   };
   
+const pool = mysql.createPool(config);
+module.exports = pool;
+
+
+let note = `CREATE TABLE note (
+    id      int(11)     unsigned NOT NULL AUTO_INCREMENT,
+    name     varchar(30) NOT NULL,
+    text    varchar(150) NOT NULL,
+    PRIMARY KEY (id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`;
+
+let note_two = `CREATE TABLE note_two (
+    id      int(11)     unsigned NOT NULL AUTO_INCREMENT,
+    name     varchar(30) NOT NULL,
+    text    varchar(150) NOT NULL,
+    PRIMARY KEY (id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`
+
+
+
+pool.query(note, async (err, result, field) => {
+    if (err) {
+        // console.log(err.message);
+    }
+    pool.query(note_two, async (err, result, field) => {
+        if (err) {
+            //   console.log(err.message);
+        }
+    });
+});
