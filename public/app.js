a1f63135919fba2221a5c216d23228836acbc086
Mikhail <stik326@mail.ru>
2022-03-24 00:19:16 +0300

    Add var form and cancel page reload

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ let $card = document.querySelector('#card')
 const submit = document.querySelector('button[type="submit"]')
 
 submit.addEventListener('click', (e) => {
-    
+    const form = document.querySelector('form')
+    e.preventDefault();
     
 });
