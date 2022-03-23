34ef7cdf0670d94af84bd6db68204c784ee80997
Mikhail <stik326@mail.ru>
2022-03-23 23:59:20 +0300

    Add get request for page output

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ app.set('views', 'views')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Get All row
+app.get('/',  async (req, res) => {
+    pool.query('SELECT * FROM note', (err, result) => {
+        if (err) throw err
+
+        res.render('index', {result})
+    })
+   
+})
