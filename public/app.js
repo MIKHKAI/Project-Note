48df0850ba444753516e0368814687fb6d7780be
Mikhail <stik326@mail.ru>
2022-03-24 00:23:16 +0300

    checking for empty empty fields and getting data from the form

--- public/app.js
+++ public/app.js
@@ -5,5 +5,15 @@ const submit = document.querySelector('button[type="submit"]')
 submit.addEventListener('click', (e) => {
     const form = document.querySelector('form')
     e.preventDefault();
+
+    const formObj = new FormData(form)
+   
+    if (form.name.value.length === '' || form.text.value.length === 0 ) {
+        return false
+    } else {
+            formObj.forEach(function(value, key){
+                dataF[key] = value;
+            });
+        }
     
-});
+})
